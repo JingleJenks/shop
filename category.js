edd91569dc5b20fd7a51414e30ce1b93d35db639
JingleJenks <sylv2439@stud.kea.dk>
2021-09-01 14:34:22 +0200

    error forEach

--- category.js
+++ category.js
@@ -1,11 +1,11 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
+const url = "https://kea-alt-del.dk/t7/api/products/subcategory";
 
 fetch(url)
     .then(function (res) {
         return res.json();
     })
     .then(function (data) {
-        showList(data);
+        showSubList(data);
     })
 
 function showSubList(data) {
@@ -13,20 +13,20 @@ function showSubList(data) {
     data.forEach(showSub);
 }
 
-// function showSub(subcategory) {
-//     console.log(subcategory);
-//     // 1.grab the template
-//     const template = document.querySelector(".categories").content;
+function showSub(subcategory) {
+    console.log(subcategory);
+    // 1.grab the template
+    const template = document.querySelector(".categories").content;
 
-//     // 2.clone it 
-//     const clone = template.cloneNode(true);
+    // 2.clone it 
+    const clone = template.cloneNode(true);
 
 
-//     // 3.change the content 
-//     clone.querySelector("span").textContent = `${product.subcategory}`;
+    // 3.change the content 
+    clone.querySelector("span").textContent = `${product.subcategory}`;
 
-//     // 4.grab the parent
-//     const parent = document.querySelector("main");
-//     // 5.append 
-//     parent.appendChild(clone);
-// }
+    // 4.grab the parent
+    const parent = document.querySelector("main");
+    // 5.append 
+    parent.appendChild(clone);
+}
