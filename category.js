6ebc2fa84420ea1c11fa53fb48a0e948bd74a262
JingleJenks <sylv2439@stud.kea.dk>
2021-09-01 15:23:05 +0200

    categories loaded

--- category.js
+++ category.js
@@ -1,6 +1,16 @@
-const url = "https://kea-alt-del.dk/t7/api/products/subcategory";
+// const url = "https://kea-alt-del.dk/t7/api/prs";
+const subCat = "https://kea-alt-del.dk/t7/api/subcategories"
 
-fetch(url)
+fetch(subCat)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        showSubList(data);
+    })
+
+
+fetch(subCat)
     .then(function (res) {
         return res.json();
     })
@@ -13,11 +23,10 @@ function showSubList(data) {
     data.forEach(showSub);
 }
 
-function showSub(subcategory) {
-    console.log(subcategory);
+function showSub(product) {
+    console.log(product);
     // 1.grab the template
-    const template = document.querySelector(".categories").content;
-
+    const template = document.querySelector(".subcategories").content;
     // 2.clone it 
     const clone = template.cloneNode(true);
 
@@ -25,8 +34,15 @@ function showSub(subcategory) {
     // 3.change the content 
     clone.querySelector("span").textContent = `${product.subcategory}`;
 
+    clone.querySelector("a").setAttribute('href', `list.html?subcategory=${product.subcategory}`)
+
+
     // 4.grab the parent
     const parent = document.querySelector("main");
     // 5.append 
     parent.appendChild(clone);
 }
+
+// function showCat(data) {
+//     console.log(data)
+// }
